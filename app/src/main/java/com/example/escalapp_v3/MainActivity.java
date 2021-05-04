29e1d7b6d7dfa1d7c43967954da62bb4b84f36b2
Franklin Almeida Campos <81038899+franklindrw@users.noreply.github.com>
2021-05-04 00:13:18 -0300

    Adc Tela de +Atividades
    Adicionei o Design da Tela de mais atividades e organizei os fragmentos em uma pasta.

--- app/src/main/java/com/example/escalapp_v3/MainActivity.java
+++ app/src/main/java/com/example/escalapp_v3/MainActivity.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.escalapp_v3.fragments.atividadesFragment;
+import com.example.escalapp_v3.fragments.folgasFragment;
+import com.example.escalapp_v3.fragments.homeFragment;
+import com.example.escalapp_v3.fragments.maisFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
