b75d1ae7b802c618636691a60d2a98e173a302b3
HappyLightning <81601280+HappyLightning@users.noreply.github.com>
2021-05-11 23:08:01 -0300

    Add files via upload

---
+++ Escalapp_v3-main/app/src/main/java/com/example/escalapp_v3/dataBase/CadastrarFuncionario.java
@@ -0,0 +1,14 @@
+package com.example.escalapp_v3.dataBase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastrarFuncionario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_funcionario);
+    }
+}
